ba307988e61ce976de7bd20aafa7c380328ea542
usersk7 <shubhamkumbhar787@gmail.com>
2019-05-14 14:42:21 +0530

    lab

---
+++ 1c/product.js
@@ -0,0 +1,12 @@
+//var product
+//var product={pname:"cococ",src:"Ubuntu-Black-Wallpaper.jpg",price:20}
+
+var products=[
+    {pid:1001,name:"Pepsi",price:20000},
+    {pid:1001,name:"coca-cola",price:20000},
+    {pid:1001,name:"vada-pavxrfcgvhbjnkm",price:20000}   
+]
+function getproduct(){
+    return products;
+}
+
